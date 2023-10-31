08467c2eabebe94a388ab66c894320692df4b4aa
Maxim Plotnikov <51321197+old-man-lib@users.noreply.github.com>
2023-11-01 02:23:33 +0300

    Rewrite bash script

--- init-another-postgresql-databases.sh
+++ init-another-postgresql-databases.sh
@@ -4,15 +4,20 @@ set -e
 set -u
 
 function create_user_and_database() {
-	local database=$(echo $1 | cut -d',' -f1)
-	local owner=$(echo $1 | cut -d',' -f2)
-	echo "Creating user and database '$database':'$owner'"
+	local database=$(echo $1 | cut -d':' -f1)
+	local owner=$(echo $1 | cut -d':' -f2)
 
-	EXISTING_USER=$(psql -U "$POSTGRES_USER" -d postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$owner'")
-	if [ "$EXISTING_USER" != "1" ]; then
-		echo "Creating user '$owner'"
-		psql -U "$POSTGRES_USER" -d postgres -c "CREATE USER $owner;"
+	if [ -z "$owner" ]; then
+		owner="$POSTGRES_USER"
+	else
+		EXISTING_USER=$(psql -U "$POSTGRES_USER" -d postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$owner'")
+		if [ "$EXISTING_USER" != "1" ]; then
+			echo "Creating user '$owner'"
+			psql -U "$POSTGRES_USER" -d postgres -c "CREATE USER $owner;"
+		fi
 	fi
+
+	echo "Creating user and database '$database':'$owner'"
 
 	EXISTING_DB=$(psql -U "$POSTGRES_USER" -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$database'")
 	if [ "$EXISTING_DB" != "1" ]; then
@@ -26,7 +31,7 @@ function create_user_and_database() {
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ':' ' '); do
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
 	done
 fi
